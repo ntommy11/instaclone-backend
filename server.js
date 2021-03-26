79b06382be52ed2cd7c68899ebf1241ea21b813c
남세현 <ntommy@MBAir.local>
2021-03-26 20:30:20 +0900

    add some examples of queries and mutations

--- server.js
+++ server.js
@@ -1,17 +1,36 @@
 import { ApolloServer, gql} from 'apollo-server';
 // The GraphQL schema
 const typeDefs = gql`
+  type Movie {
+    title:String
+    year:Int
+  }
   type Query {
-    "A simple type for getting started!"
-    hello: String
+    movies: [Movie]
+    movie:Movie
+  }
+  type Mutation {
+    createMovie(title:String!):Boolean
+    deleteMovie(title:String!):Boolean
   }
 `;
 
 // A map of functions which return data for the schema.
 const resolvers = {
   Query: {
-    hello: () => 'hii',
+    movies: () => [],
+    movie: () => ({title:"hello", year:2021}),
   },
+  Mutation: {
+    createMovie: (_, {title}) => {
+      console.log(title);
+      return true;
+    },
+    deleteMovie: (_, {title}) => {
+      console.log(title);
+      return true;
+    },
+  }
 };
 
 const server = new ApolloServer({
